e4fd0eb5530e28317521e656061e46348b826ffc
Claas Anders Rathje <claasr@thje.net>
2024-09-04 21:40:52 +0200

    Playground/checksum again (#1)
    * Try another checksum
    
    * Fix c 'n p error for checksum from js
    
    * Parse packages inside 3package burst individually and notify only in case new (and different) data arrived
    
    * Rewritten to get rid of the bit in byte storage. Added more samples for dry play and reference to reveng.

--- src/tfa433.h
+++ src/tfa433.h
@@ -15,9 +15,15 @@ compatible base station 30.3055.01 https://www.tfa-dostmann.de/produkt/funk-ther
 #include "WProgram.h"
 #endif
 
+#ifdef ESP32
 // we do not like static interrupt handlers, ESP32 allows us to use function interrupts
 #include <FunctionalInterrupt.h>
+#else
+#define ARDUINO_ISR_ATTR 
+#endif
 
+// in case you do not have a device handy and still need to play around
+// uncomment the next line to get some stuff going while offline
 // #define __TFA_ENABLE_DRY_TEST 1
 
 // actual ~515
@@ -25,18 +31,14 @@ compatible base station 30.3055.01 https://www.tfa-dostmann.de/produkt/funk-ther
 // actual ~515
 #define SYNCPULSELENMAX 600
 #define MAXPULSELEN 3000
-#define _PAK_SIZE 135
-#define _BUFF_SIZE _PAK_SIZE + 5
-
-// #define dbg(s) Serial.println(s)
-#define dbg(s)
+#define _PAK_SIZE 40
+#define _BUFF_SIZE _PAK_SIZE / 8
 
 typedef struct tfaResult
 {
-	byte id;
-	byte channel;
+	uint8_t id;
+	uint8_t channel;
 	int16_t temperature;
-	unsigned long packageMS;
 } tfaResult;
 
 class TFA433
@@ -46,29 +48,26 @@ public:
 	void start(int pin);
 	void stop();
 	bool isDataAvailable();
-	void getData(byte &id, byte &channel, int16_t &temperature, unsigned long &packageMS);
+	void getData(uint8_t &id, uint8_t &channel, int16_t &temperature);
 	tfaResult getData();
 #ifdef __TFA_ENABLE_DRY_TEST
 	void _play_dry();
 #endif
 
 private:
-	volatile bool _avail, _inSync, _inPacket;
-	byte _buff[_BUFF_SIZE];
-	volatile byte _buffEnd;
+	volatile bool _avail, _inPacket;
+	uint8_t _buff[_BUFF_SIZE];
+	volatile uint8_t _buffEnd, _syncCount;
 
 	uint8_t _lastPinValue;
-	unsigned long _lastPulseLen, _lastHighUsec, _lastUsec;
-
-	byte _lastBuff[_BUFF_SIZE];
-	byte _pin;
+	unsigned long _lastPulseLen, _lastLowUsec, _lastUsec;
+	unsigned long _lastPackageArrived;
+	uint8_t _pin;
 
 	void _init();
 	void ARDUINO_ISR_ATTR _handler();
 	bool ARDUINO_ISR_ATTR _handler_internal(unsigned long uSec, uint8_t pinValue);
-	int _binToDecRev(byte *binary, int s, int e);
-	int _binToDec(byte *binary, int s, int e);
-
+	uint8_t _checksum(uint8_t *addr, uint8_t len);
 	tfaResult _values;
 };
 
